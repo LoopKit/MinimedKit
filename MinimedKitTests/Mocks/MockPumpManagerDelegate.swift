665c62413ac6cce84fb0396b63fcee83c58b5ea4
Nathaniel Hamming <nate@hmtconsulting.org>
2023-05-24 10:14:26 -0300

    updated based on PR comment

--- MinimedKitTests/Mocks/MockPumpManagerDelegate.swift
+++ MinimedKitTests/Mocks/MockPumpManagerDelegate.swift
@@ -52,7 +52,7 @@ class MockPumpManagerDelegate: PumpManagerDelegate {
 
     func pumpManagerDidUpdateState(_ pumpManager: PumpManager) {}
     
-    func pumpManager(_ pumpManager: PumpManager, didUpdateBasalRateSchedule basalRateSchedule: BasalRateSchedule) {}
+    func pumpManager(_ pumpManager: PumpManager, didRequestBasalRateScheduleChange basalRateSchedule: BasalRateSchedule, completion: @escaping (Error?) -> Void) { }
 
     func startDateToFilterNewPumpEvents(for manager: PumpManager) -> Date {
         return historyFetchStartDate
