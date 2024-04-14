44b25aaafba88dc7f511e36fc8d709df10dc9380
marionbarker <marionbarker@earthlink.net>
2024-04-14 14:36:08 -0700

    use environment appName instead of hardcoded Loop

--- MinimedKitUI/RadioSelectionTableViewController.swift
+++ MinimedKitUI/RadioSelectionTableViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import LoopKitUI
 import MinimedKit
+import SwiftUI
 
 
 extension RadioSelectionTableViewController: IdentifiableClass {
@@ -26,21 +27,23 @@ extension RadioSelectionTableViewController: IdentifiableClass {
 
     static func batteryChemistryType(_ value: MinimedKit.BatteryChemistryType) -> T {
         let vc = T()
+        @Environment(\.appName) var appName
 
         vc.selectedIndex = value.rawValue
         vc.options = (0..<2).compactMap({ BatteryChemistryType(rawValue: $0) }).map { String(describing: $0) }
-        vc.contextHelp = LocalizedString("Alkaline and Lithium batteries decay at differing rates. Alkaline tend to have a linear voltage drop over time whereas lithium cell batteries tend to maintain voltage until halfway through their lifespan. Under normal usage in a Non-MySentry compatible Minimed (x22/x15) insulin pump running Loop, Alkaline batteries last approximately 4 to 5 days. Lithium batteries last between 1-2 weeks. This selection will use different battery voltage decay rates for each of the battery chemistry types and alert the user when a battery is approximately 8 to 10 hours from failure.", comment: "Instructions on selecting battery chemistry type")
+        vc.contextHelp = String(format: LocalizedString("Alkaline and Lithium batteries decay at differing rates. Alkaline tend to have a linear voltage drop over time whereas lithium cell batteries tend to maintain voltage until halfway through their lifespan. Under normal usage in a Non-MySentry compatible Minimed (x22/x15) insulin pump running %1$@, Alkaline batteries last approximately 4 to 5 days. Lithium batteries last between 1-2 weeks. This selection will use different battery voltage decay rates for each of the battery chemistry types and alert the user when a battery is approximately 8 to 10 hours from failure.", comment: "Instructions on selecting battery chemistry type (1: appName)"), appName)
 
         return vc
     }
 
     static func useMySentry(_ value: Bool) -> T {
         let vc = T()
+        @Environment(\.appName) var appName
 
         vc.selectedIndex = value ? 0 : 1
             
         vc.options = ["Use MySentry", "Do not use MySentry"]
-        vc.contextHelp = LocalizedString("Medtronic pump models 523, 723, 554, and 754 have a feature called 'MySentry' that periodically broadcasts the reservoir and pump battery levels.  Listening for these broadcasts allows Loop to communicate with the pump less frequently, which can increase pump battery life.  However, when using this feature the RileyLink stays awake more of the time and uses more of its own battery.  Enabling this may lengthen pump battery life, while disabling it may lengthen RileyLink battery life. This setting is ignored for other pump models.", comment: "Instructions on selecting setting for MySentry")
+        vc.contextHelp = String(format: LocalizedString("Medtronic pump models 523, 723, 554, and 754 have a feature called 'MySentry' that periodically broadcasts the reservoir and pump battery levels.  Listening for these broadcasts allows %1$@ to communicate with the pump less frequently, which can increase pump battery life.  However, when using this feature the RileyLink stays awake more of the time and uses more of its own battery.  Enabling this may lengthen pump battery life, while disabling it may lengthen RileyLink battery life. This setting is ignored for other pump models.", comment: "Instructions on selecting setting for MySentry (1: appName)"), appName)
 
         return vc
     }
